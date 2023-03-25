c21f87c37621b62704bd0c74fe59da1d81d68847
narah10 <kwa18001@byui.edu>
2023-03-25 12:58:45 -0600

    dashboard done

--- src/page/dashboard/dahsboard.js
+++ src/page/dashboard/dahsboard.js
@@ -6,13 +6,17 @@ import dash from "./dashboard.module.css";
 import articleImage from "./articleimage.svg";
 import Memorycard from "../rememberance/memerycard";
 import memoryimg from "../rememberance/placeholder.svg";
+import circle from "./circle.svg";
+import elderly from "./elderly.svg";
+import elderly2 from "./elderly2.svg";
+import elderly3 from "./elderly3.svg";
 
 export default function Dashboard() {
   return (
     <div>
       <Navigation
         body={
-          <div>
+          <div class={dash.overall}>
             <div>
               <Banner
                 title="Welcome!"
@@ -20,32 +24,34 @@ export default function Dashboard() {
               />
             </div>
             <div class={dash.memorials}>
-              <p>Memorials</p>
+              <p class={dash.littleHeader}>Memorials</p>
               <div class={dash.memories}>
                 <Memorycard
                   class={dash.eachItem}
-                  imageurl={memoryimg}
-                  name="Name of Person"
-                  comment='"Quote from the Person"'
+                  imageurl={elderly}
+                  name="Jovita Ceron"
+                  comment="“A faithful woman”"
                 />
                 <Memorycard
                   class={dash.eachItem}
-                  imageurl={memoryimg}
-                  name="Name of Person"
-                  comment='"Quote from the Person"'
+                  imageurl={elderly2}
+                  name="Larae Murdock"
+                  comment="“A spiritual woman”"
                 />
                 <Memorycard
                   class={dash.eachItem}
-                  imageurl={memoryimg}
-                  name="Name of Person"
-                  comment='"Quote from the Person"'
+                  imageurl={elderly3}
+                  name="Segundo Carazas"
+                  comment="“A happy men”"
                 />
               </div>
             </div>
 
             <div class={dash.bottomPart}>
               <div class={dash.articlecontainer}>
-                <p id={dash.heading}>Article of the Day</p>
+                <p id={dash.heading} class={dash.littleHeader}>
+                  Article of the Day
+                </p>
                 <div class={dash.articlePart}>
                   <img src={articleImage} id={dash.img} />
                   <div class={dash.info}>
@@ -56,24 +62,33 @@ export default function Dashboard() {
                       aftermath: grief, the natural reaction to loss of a loved
                       one.
                     </p>
-                    <p>LEARN MORE</p>
+                    <p id={dash.learnmore}>LEARN MORE</p>
                   </div>
                 </div>
               </div>
               <div class={dash.comingUp}>
-                <p>Memorable Dates</p>
+                <p class={dash.littleHeader}>Memorable Dates</p>
                 <div class={dash.dates}>
-                  <div>
-                    <p>Date</p>
-                    <p>Event</p>
+                  <div class={dash.dateDetails}>
+                    <img src={circle} class={dash.round} />
+                    <div>
+                      <p class={dash.dateColor}>Tuesday, Feb 10</p>
+                      <p class={dash.eventColor}>Carlos Escalona Birthday</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>Date</p>
-                    <p>Event</p>
+                  <div class={dash.dateDetails}>
+                    <img src={circle} class={dash.round} />
+                    <div>
+                      <p class={dash.dateColor}>Tuesday, Feb 14</p>
+                      <p class={dash.eventColor}>Jovita’s Birthday</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>Date</p>
-                    <p>Event</p>
+                  <div class={dash.dateDetails}>
+                    <img src={circle} class={dash.round} />
+                    <div>
+                      <p class={dash.dateColor}>Tuesday, Feb 10</p>
+                      <p class={dash.eventColor}>Larae’s Anniversary </p>
+                    </div>
                   </div>
                 </div>
               </div>
